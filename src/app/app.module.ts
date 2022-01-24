495ea11892249d76a4b59016118310388e0c86ab
dancer1325 <alfredotic0809@gmail.com>
2022-01-24 13:28:57 +0100

    Remove invalid credential

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
 
-
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { SearchComponent } from './components/search/search.component';
-import { ArtistaComponent } from './components/artista/artista.component';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./components/home/home.component";
+import { SearchComponent } from "./components/search/search.component";
+import { ArtistaComponent } from "./components/artista/artista.component";
+import { NavbarComponent } from "./components/shared/navbar/navbar.component";
 
 // Importar rutas
-import { ROUTES } from './app.routes';
+import { ROUTES } from "./app.routes";
 
 // Pipes
-import { NoimagePipe } from './pipes/noimage.pipe';
-import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { NoimagePipe } from "./pipes/noimage.pipe";
+import { DomseguroPipe } from "./pipes/domseguro.pipe";
 
-
-import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
-import { LoadingComponent } from './components/shared/loading/loading.component';
-
+import { TarjetasComponent } from "./components/tarjetas/tarjetas.component";
+import { LoadingComponent } from "./components/shared/loading/loading.component";
 
 @NgModule({
   declarations: [
@@ -33,14 +30,14 @@ import { LoadingComponent } from './components/shared/loading/loading.component'
     NoimagePipe,
     DomseguroPipe,
     TarjetasComponent,
-    LoadingComponent
+    LoadingComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    RouterModule.forRoot( ROUTES, { useHash: true } )
+    HttpClientModule, // Configure dependency injector for HttpClient
+    RouterModule.forRoot(ROUTES, { useHash: true }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,16 +1,15 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 
-import { map } from 'rxjs/operators';
+import { map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class SpotifyService {
-
   bearerToken: string;
-  tokenUrl: string = 'https://accounts.spotify.com/api/token';
-  tokenBody = new URLSearchParams().set('grant_type', 'client_credentials');
+  tokenUrl: string = "https://accounts.spotify.com/api/token";
+  tokenBody = new URLSearchParams().set("grant_type", "client_credentials");
   // tokenBody ={
   //   'grant_type': 'client_credentials'
   // };
@@ -24,64 +23,56 @@ export class SpotifyService {
   // });
   // tokenBody = new FormData().append('grant_type', 'client_credentials');
   headers2 = {
-    'Authorization': 'Basic ODEyZjBkOWNlOGJjNDlmNGI1Y2E3OWRkOWRiNzhlNGI6Y2E0YWUzYjM4NzdmNDBlMWI0ZWUyYjE5NjBhYTk4MmQ='
+    Authorization:
+      "Basic ODEyZjBkOWNlOGJjNDlmNGI1Y2E3OWRkOWRiNzhlNGI6Y2E0YWUzYjM4NzdmNDBlMWI0ZWUyYjE5NjBhYTk4MmQ=",
     // ,
     // 'Content-Type': 'application/x-www-form-urlencoded'
   };
-  // headers2 = new HttpHeaders({
-  //   'Authorization': 'Basic ODEyZjBkOWNlOGJjNDlmNGI1Y2E3OWRkOWRiNzhlNGI6Y2E0YWUzYjM4NzdmNDBlMWI0ZWUyYjE5NjBhYTk4MmQ='
-  // });
 
   constructor(private http: HttpClient) {
-    console.log('Spotify Service Listo');
-    this.http.post<any>(this.tokenUrl, this.tokenBody, {headers: this.headers2 } ).subscribe(data => {
-      this.bearerToken = data.token_type + " " + data.access_token;
-    });
+    console.log("Spotify Service Listo");
+    this.http
+      .post<any>(this.tokenUrl, this.tokenBody, { headers: this.headers2 })
+      .subscribe((data) => {
+        this.bearerToken = data.token_type + " " + data.access_token;
+      });
     // this.http.post<any>(this.tokenUrl, this.tokenBody, this.headers2 ).subscribe(data => {
     //   this.bearerToken = data.token_type + " " + data.access_token;
     // });
     console.log(this.bearerToken);
   }
 
-  getQuery( query: string ) {
-
-    const url = `https://api.spotify.com/v1/${ query }`;
+  getQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${query}`;
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQAKRQm6UKswq9g1NTWgDw0f0qUviSo0fNahJ6r8yq6yUCUv7R56WiWQT0RXoL_pmT1Ktzb3SiTah2TJMFs'
+      Authorization:
+        "Bearer BQAKRQm6UKswq9g1NTWgDw0f0qUviSo0fNahJ6r8yq6yUCUv7R56WiWQT0RXoL_pmT1Ktzb3SiTah2TJMFs",
     });
 
     return this.http.get(url, { headers });
-
   }
-
 
   getNewReleases() {
-
-    return this.getQuery('browse/new-releases?limit=20')
-              .pipe( map( data => data['albums'].items ));
-
+    return this.getQuery("browse/new-releases?limit=20").pipe(
+      map((data) => data["albums"].items)
+    );
   }
 
-  getArtistas( termino: string ) {
-
-    return this.getQuery(`search?q=${ termino }&type=artist&limit=15`)
-                .pipe( map( data => data['artists'].items));
-
+  getArtistas(termino: string) {
+    return this.getQuery(`search?q=${termino}&type=artist&limit=15`).pipe(
+      map((data) => data["artists"].items)
+    );
   }
 
-  getArtista( id: string ) {
-
-    return this.getQuery(`artists/${ id }`);
-                // .pipe( map( data => data['artists'].items));
-
+  getArtista(id: string) {
+    return this.getQuery(`artists/${id}`);
+    // .pipe( map( data => data['artists'].items));
   }
 
-  getTopTracks( id: string ) {
-
-    return this.getQuery(`artists/${ id }/top-tracks?country=us`)
-                .pipe( map( data => data['tracks']));
-
+  getTopTracks(id: string) {
+    return this.getQuery(`artists/${id}/top-tracks?country=us`).pipe(
+      map((data) => data["tracks"])
+    );
   }
-
 }
